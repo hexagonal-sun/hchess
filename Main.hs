a0421174334ed3b2724583115c49ce3fcaff55fd
Matthew Leach <matthew@mattleach.net>
2020-08-16 21:51:06 +0100

    [MoveGen] repeatedly apply movement for different pieces

--- Main.hs
+++ Main.hs
@@ -9,5 +9,9 @@ main :: IO ()
 main = do
   let board = startingBoard
   pp board
+  putStrLn "Bishop"
   let boards = pseudoMoveGen board (FC, R1)
+  mapM_ pp boards
+  putStrLn "Pawn"
+  let boards = pseudoMoveGen board (FB, R2)
   mapM_ pp boards
--- MoveGen.hs
+++ MoveGen.hs
@@ -10,39 +10,50 @@ import Board
 import Locus
 import Piece
 
-dirRep :: [Direction] -> [[Direction]]
-dirRep d = map (\n -> concat $ replicate n d) [1..7]
+data PieceDirection = PieceDirection [[Direction]] Bool
 
 orthoDirs :: [[Direction]]
-orthoDirs = dirRep [North] ++ dirRep[East] ++
-            dirRep[South]  ++ dirRep[West]
+orthoDirs = [[North], [East], [South], [West]]
 
 diagDirs :: [[Direction]]
-diagDirs = dirRep[North,East] ++ dirRep[North,West] ++
-           dirRep[South,East] ++ dirRep[South,West]
+diagDirs = [[North, East], [North, West],
+            [South, East], [South, West]]
 
+kindDirections' :: Locus -> Piece -> PieceDirection
+kindDirections' (_, R2) (Piece White Pawn) = PieceDirection [[North, North], [North]] False
+kindDirections' _       (Piece White Pawn) = PieceDirection [[North]] False
+kindDirections' (_, R7) (Piece Black Pawn) = PieceDirection [[South], [South, South]] False
+kindDirections' _       (Piece Black Pawn) = PieceDirection [[South]] False
+kindDirections' _       (Piece _ Knight)   = PieceDirection [[North, North, East], [North, North, West],
+                                                             [South, South, East], [South, South, West]]
+                                                            False
+kindDirections' _       (Piece _ King)     = PieceDirection orthoDirs False
+kindDirections' _       (Piece _ Rook)     = PieceDirection orthoDirs True
+kindDirections' _       (Piece _ Bishop)   = PieceDirection diagDirs True
+kindDirections' _       (Piece _ Queen)    = PieceDirection (orthoDirs ++ diagDirs) True
 
-kindDirections' :: Locus -> Piece -> [[Direction]]
-kindDirections' (_, R2) (Piece White Pawn) = [[North, North], [North]]
-kindDirections' _       (Piece White Pawn) = [[North]]
-kindDirections' (_, R7) (Piece Black Pawn) = [[South], [South, South]]
-kindDirections' _       (Piece Black Pawn) = [[South]]
-kindDirections' _       (Piece _ Knight)   = [[North, North, East], [North, North, West],
-                                              [South, South, East], [South, South, West]]
-kindDirections' _       (Piece _ King)     = [[North], [East], [South], [West]]
-kindDirections' _       (Piece _ Rook)     = orthoDirs
-kindDirections' _       (Piece _ Bishop)   = diagDirs
-kindDirections' _       (Piece _ Queen)    = orthoDirs ++ diagDirs
-
-kindDirections :: Locus -> Maybe Piece -> [[Direction]]
+kindDirections :: Locus -> Maybe Piece -> PieceDirection
 kindDirections l (Just p) = kindDirections' l p
-kindDirections _ Nothing  = []
+kindDirections _ Nothing  = PieceDirection [] False
+
+applyDirections' :: Maybe Locus -> [Direction] -> [Maybe Locus]
+applyDirections' Nothing _ = []
+applyDirections' (Just l) dir = x:(applyDirections' x dir)
+  where x = move l dir
+
+applyDirections :: Locus -> [Direction] -> [Locus]
+applyDirections l d = catMaybes $ applyDirections' (Just l) d
+
+getMoves :: Locus -> PieceDirection -> [Locus]
+getMoves l (PieceDirection dirs False) = catMaybes $ map (\d -> move l d) dirs
+getMoves l (PieceDirection dirs True)  = concat $ map (\d -> applyDirections l d) dirs
 
 pseudoMoveGen' :: BoardState -> Locus -> Locus -> BoardState
 pseudoMoveGen' board from to = board // [(from, Nothing),
                                          (to,   board ! from)]
 
+
 pseudoMoveGen :: BoardState -> Locus -> [BoardState]
 pseudoMoveGen board l = map (pseudoMoveGen' board l) moves
-  where directions = kindDirections l $ board ! l
-        moves      = catMaybes $ map (move l) directions
+  where pd = kindDirections l $ board ! l
+        moves = getMoves l pd
