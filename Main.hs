c3a2725c2a241d0610530f9421c3d5a631b5b0f0
Matthew Leach <matthew@mattleach.net>
2020-08-21 21:53:00 +0100

    [Main] implement perft

--- Main.hs
+++ Main.hs
@@ -1,11 +1,18 @@
 module Main where
 
-import Locus
+import Board
 import PrettyPrint
 import MoveGen
 import Game
+import Data.Ix
+
+perft :: Int -> GameState -> Int
+perft 0 _  = 1
+perft n state = sum $ map (perft (n - 1)) subStates
+  where subStates = concatMap (moveGen state) $ range boardBounds
 
 main :: IO ()
 main = do
   let g = newGame
   pp g
+  putStrLn $ "pert 0: " ++ (show $ perft 3 g)
