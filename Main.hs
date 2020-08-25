8940b93939af6bd12908c3d509eb77acf287e19c
Matthew Leach <matthew@mattleach.net>
2020-08-25 20:14:32 +0100

    [Main] implement perft split
    Show top-level counts during perft to aid in debugging

--- Main.hs
+++ Main.hs
@@ -3,11 +3,19 @@ module Main where
 import MoveGen
 import Game
 
-perft :: Int -> GameState -> Int
-perft 0 _  = 1
-perft n state = sum $ map (\(_,_,s) -> perft (n - 1) s) $ moveGen state
+perft' :: Int -> GameState -> Int
+perft' 0 _  = 1
+perft' n state = sum $ map (\(_,_,s) -> perft' (n - 1) s) $ moveGen state
+
+perft :: GameState -> Int -> IO ()
+perft state n = do
+  let states = moveGen state
+  let perfts = map (\(f, t, s) -> (f,t, perft' (n - 1) s)) states
+  mapM_ print perfts
+  let total = foldl (\x (_,_,p) -> x + p) 0 perfts
+  putStrLn $ "Perft " ++ show n ++ " total: " ++ show total
 
 main :: IO ()
 main = do
   let g = newGame
-  mapM_ (\n -> putStrLn $ "perft " ++ show n ++ ": " ++ show (perft n g)) [1..4]
+  mapM_ (perft g) [1..4]
