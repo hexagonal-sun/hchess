1f494e137f45f29d8dbe5c3bc652a092c99bdf59
Matthew Leach <matthew@mattleach.net>
2020-09-04 16:37:33 +0100

    [Game] make castling moves
    If the king makes a castling move, also move the rook.

--- Game.hs
+++ Game.hs
@@ -29,11 +29,21 @@ createBoard :: BoardState -> Locus -> Locus -> BoardState
 createBoard b from to = b // [(from, Nothing),
                               (to,   b ! from)]
 
+makeMove' :: BoardState -> PieceKind -> Locus -> Locus -> BoardState
+makeMove' b piece from@(ff,fr) to@(tf,_) = foldr (\(f,t) b' -> createBoard b' f t) b moves
+  where m = [(from,to)]
+        moves = case piece of
+          King -> case (ff,tf) of
+            (FE,FG) -> ((FH,fr),(FF,fr)):m
+            (FE,FC) -> ((FA,fr),(FD,fr)):m
+            _ -> m
+          _ -> m
+
 makeMove :: GameState -> Locus -> Locus -> Maybe GameState
 makeMove (GameState b nextColour wK bK cr) from@(file,_) to =
   case b ! from of
     Nothing -> Nothing
-    Just (Piece c k)  -> Just $ GameState (createBoard b from to) (switch nextColour) nwK nbK ncr
+    Just (Piece c k)  -> Just $ GameState (makeMove' b k from to) (switch nextColour) nwK nbK ncr
       where nwK = if from == wK then to else wK
             nbK = if from == bK then to else bK
             ncr = case k of
