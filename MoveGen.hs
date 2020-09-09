49d6a5d6e67749a54c10ce224caee1a5dab6f325
Matthew Leach <matthew@mattleach.net>
2020-09-09 21:15:49 +0100

    [MoveGen] rework
    Rework movegen to differentiate ray types, so that we can tell the
    difference between Attack, Move and MoveAndAttack rays.

--- MoveGen.hs
+++ MoveGen.hs
@@ -11,7 +11,14 @@ import Game
 import Locus
 import Piece
 
-data MovementSpec = MovementSpec [Vector] Int
+data RaySpec = RaySpec [Vector] Int
+
+data MovementSpec = DifferingAttack RaySpec RaySpec |
+                    ConsistentAttack RaySpec
+
+data CandidateMoves = Attack      [Ray] |
+                      Move        [Ray] |
+                      AttackMove  [Ray]
 
 orthoVecs :: [Vector]
 orthoVecs = [[North], [East], [South], [West]]
@@ -20,67 +27,85 @@ diagVecs :: [Vector]
 diagVecs = [[North, East], [North, West],
             [South, East], [South, West]]
 
-isOccupied :: BoardState -> Maybe Locus -> Bool
-isOccupied _ Nothing = False
-isOccupied b (Just l) = case b ! l of
-  Nothing -> False
-  Just _ -> True
-
-canAttack :: GameState -> Colour -> Maybe Locus -> Bool
-canAttack _ _ Nothing = False
-canAttack game c (Just l) = attackP || attackEP
-  where attackEP = EP.isEPLocus (enPassant game) l
-        attackP  = case (board game) ! l of
-          Nothing -> False
-          Just (Piece pC _) -> pC == switch c
-
-kindVectors :: GameState -> Locus -> Piece -> MovementSpec
-kindVectors game l@(_, rank) (Piece c Pawn) = MovementSpec (y:x) n
+kindVectors :: Locus -> Piece -> MovementSpec
+kindVectors (_, rank) (Piece c Pawn) = DifferingAttack moveVec attackVec
   where dir = if c == White then North else South
-        moveOcc = isOccupied (board game) . move l
-        moveAtt = canAttack game c . move l
         atHome = case (rank,c) of
           (R7,Black) -> True
           (R2,White) -> True
           _          -> False
-        n = case atHome of
-          True -> if moveOcc [dir,dir] then 1 else 2
-          _    -> 1
-        attackVecs = [[dir,East],[dir,West]]
-        x = filter moveAtt attackVecs
-        y = [dir | not $ moveOcc [dir]]
+        moveVec = RaySpec [[dir]] $ if atHome then 2 else 1
+        attackVec = RaySpec [[dir,East],[dir,West]] 1
 
-kindVectors _ _ (Piece _ Knight)   = MovementSpec [[North, North, East], [North, North, West],
-                                                  [South, South, East], [South, South, West],
-                                                  [East, East, North], [East, East, South],
-                                                  [West, West, North], [West, West, South]]
-                                   1
-kindVectors _ _ (Piece _ King)     = MovementSpec (orthoVecs ++ diagVecs) 1
-kindVectors _ _ (Piece _ Rook)     = MovementSpec orthoVecs repeatEntireSpan
-kindVectors _ _ (Piece _ Bishop)   = MovementSpec diagVecs repeatEntireSpan
-kindVectors _ _ (Piece _ Queen)    = MovementSpec (orthoVecs ++ diagVecs) repeatEntireSpan
+kindVectors _ (Piece _ Knight)   = ConsistentAttack ray
+  where ray = RaySpec [[North, North, East],  [North, North, West],
+                       [South, South, East], [South, South, West],
+                       [East, East, North],  [East, East, South],
+                       [West, West, North],  [West, West, South]] 1
 
-getRays :: GameState -> Locus -> Piece -> [Ray]
-getRays g l p = case kindVectors g l p of
-  (MovementSpec vecs repeatVec) -> map (applyVector l repeatVec) vecs
+kindVectors _ (Piece _ King)     = ConsistentAttack ray
 
-pruneRay :: BoardState -> Colour -> Ray -> Ray
-pruneRay _ _ []  = []
-pruneRay b c (nl:ray) = case b ! nl of
-                          Nothing -> nl:pruneRay b c ray
-                          Just (Piece otherColour _) -> [nl | otherColour /= c]
+  where ray = RaySpec (orthoVecs ++ diagVecs) 1
+kindVectors _ (Piece _ Rook)     = ConsistentAttack ray
+  where ray = RaySpec orthoVecs repeatEntireSpan
+kindVectors _ (Piece _ Bishop)   = ConsistentAttack ray
+  where ray = RaySpec diagVecs repeatEntireSpan
+kindVectors _ (Piece _ Queen)    = ConsistentAttack ray
+  where ray = RaySpec (orthoVecs ++ diagVecs) repeatEntireSpan
 
-isRayAttacking :: BoardState -> Piece -> Ray -> Bool
-isRayAttacking _ _ [] = False
-isRayAttacking b p (l:r)  = case b ! l of
-  Nothing -> isRayAttacking b p r
-  Just p' -> p == p'
+getRays :: Locus -> Piece -> [CandidateMoves]
+getRays l p = case kindVectors l p of
+  ConsistentAttack (RaySpec vecs repeatVec) -> [AttackMove $ map (applyVector l repeatVec) vecs]
+  DifferingAttack (RaySpec mVecs mRepeatVec) (RaySpec aVecs aRepeatVec) ->
+    [Attack $ map(applyVector l aRepeatVec) aVecs,
+     Move   $ map(applyVector l mRepeatVec) mVecs]
+
+canAttack :: GameState -> Colour -> Locus -> Bool
+canAttack game c l = EP.isEPLocus (enPassant game) l || attackablePiece
+  where
+    attackablePiece = case board game ! l of
+      Nothing -> False
+      Just (Piece pc _) -> pc /= c
+
+pruneAttackRay :: GameState -> Colour -> Ray -> Ray
+pruneAttackRay game c ray = filter (canAttack game c) ray
+
+isOccupied :: GameState -> Locus -> Bool
+isOccupied game l = case board game ! l of
+  Nothing -> False
+  Just _ -> True
+
+pruneMoveRay :: GameState -> Ray -> Ray
+pruneMoveRay game ray = takeWhile (not . isOccupied game) ray
+
+pruneAttackMoveRay :: GameState -> Colour -> Ray -> Ray
+pruneAttackMoveRay _ _ []  = []
+pruneAttackMoveRay game c (l:ls) = case board game ! l of
+  Nothing                    -> l:pruneAttackMoveRay game c ls
+  Just (Piece otherColour _) -> [l | otherColour /= c]
+
+pruneMoves :: GameState -> Colour -> [CandidateMoves] -> Ray
+pruneMoves game c = concatMap (\cm -> case cm of
+  (Attack rays)     -> concatMap (pruneAttackRay game c) rays
+  (Move rays)       -> concatMap (pruneMoveRay game) rays
+  (AttackMove rays) -> concatMap (pruneAttackMoveRay game c) rays)
+
+isRayAttacking' :: BoardState -> Piece -> Ray -> Bool
+isRayAttacking' _ _ []    = False
+isRayAttacking' b p@(Piece c k) (l:r) = case b ! l of
+  Nothing -> isRayAttacking' b p r
+  Just (Piece c' k') ->  c' == switch c && k' == k
+
+isRayAttacking :: BoardState -> Piece -> CandidateMoves -> Bool
+isRayAttacking _ _ (Move _)          = False
+isRayAttacking b p (Attack rays)     = any (isRayAttacking' b p) rays
+isRayAttacking b p (AttackMove rays) = any (isRayAttacking' b p) rays
 
 isSquareUnderAttack' :: GameState -> Locus -> Piece -> Bool
-isSquareUnderAttack' game l p = any (isRayAttacking (board game) p) $ getRays game l p
+isSquareUnderAttack' game l p = any (isRayAttacking (board game) p) $ getRays l p
 
 isSquareUnderAttack :: GameState -> Colour -> Locus -> Bool
-isSquareUnderAttack g c l = any (isSquareUnderAttack' g l . Piece c) allKinds
+isSquareUnderAttack g c l = any (isSquareUnderAttack' g l . Piece (switch c)) allKinds
 
 isInCheck :: Colour -> GameState -> Bool
 isInCheck c game = isSquareUnderAttack game (switch c) kingPos
@@ -93,7 +118,7 @@ genCastlingMoves' game (CastlingRights side colour) =
       rank     = if colour == White then R1 else R8
       from     = (FE,rank)
       obsRay   = applyVector from obsRaySz [dir]
-      isOcc    = any (isOccupied (board game) . Just) obsRay
+      isOcc    = any (isOccupied game) obsRay
       checkRay = applyVector from 2 [dir]
       to       = last checkRay
       isCheck  = any (isSquareUnderAttack game (switch $ toMove game)) checkRay
@@ -111,8 +136,8 @@ moveGen' game from = case board game ! from of
   Just p@(Piece c k) -> mapMaybe (\to -> case makeMove game from to of
                                      Nothing -> Nothing
                                      Just state -> Just (from, to, state)) validMoves'
-    where rays = getRays game from p
-          validMoves = concatMap (pruneRay (board game) c) rays ++ if k == King then genCastlingMoves game else []
+    where rays = getRays from p
+          validMoves = pruneMoves game c rays ++ if k == King then genCastlingMoves game else []
           validMoves' = if k == King then filter (not . isSquareUnderAttack game (switch c)) validMoves else validMoves
 
 moveGen :: GameState -> [(Locus, Locus, GameState)]
