9956302c36c1cee6b9cddc420f85bd6dc6283525
Matthew Leach <matthew@mattleach.net>
2020-09-11 18:09:12 +0100

    [Perft] expose the base function PerftInt
    Expose the main function of perft that allows the integer value to be
    returned.

--- Perft.hs
+++ Perft.hs
@@ -1,4 +1,5 @@
 module Perft (
+  perftInt,
   perft,
   perftSplit) where
 
@@ -49,14 +50,14 @@ pMove = do
   to   <- pLocus
   return (from,to)
 
-perft' :: Int -> GameState -> Int
-perft' 0 _  = 1
-perft' n state = sum $ map (\(_,_,s) -> perft' (n - 1) s) $ moveGen state
+perftInt :: Int -> GameState -> Int
+perftInt 0 _  = 1
+perftInt n state = sum $ map (\(_,_,s) -> perftInt (n - 1) s) $ moveGen state
 
 perft :: GameState -> Int -> IO ()
 perft state n = do
   let states = moveGen state
-  let perfts = map (\(f, t, s) -> (f,t, perft' (n - 1) s)) states
+  let perfts = map (\(f, t, s) -> (f,t, perftInt (n - 1) s)) states
   mapM_ (\(from,to,num) -> pp from >> pp to >> putStr ": " >> print num) perfts
   let total = foldl (\x (_,_,p) -> x + p) 0 perfts
   putStrLn $ "Perft " ++ show n ++ " total: " ++ show total
