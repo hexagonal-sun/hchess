ae9a5e6fa7fb678eb471dbeab0137b5f2294f508
Matthew Leach <matthew@mattleach.net>
2020-10-03 21:40:25 +0100

    [Search] implement alpha beta

--- src/Search.hs
+++ src/Search.hs
@@ -6,21 +6,28 @@ import Move
 import Data.Tree
 import MoveGen
 import Evaluate
-import Data.Ord
 
-data EvaluatedNode a = EvaluatedNode GameState a
+data ABNode a = EvaluatedNode GameState a | Inf | NegInf
 
-instance (Eq a) => Eq (EvaluatedNode a) where
+instance (Eq a) => Eq (ABNode a) where
   (==) (EvaluatedNode _ x) (EvaluatedNode _ y) = x == y
- 
-instance (Ord a) => Ord (EvaluatedNode a) where
-  compare (EvaluatedNode _ x) (EvaluatedNode _ y) = compare x y
+  (==) _ _    = False
 
-evalNode :: GameState -> EvaluatedNode Double
+instance (Ord a) => Ord (ABNode a) where
+  compare (EvaluatedNode _ x) (EvaluatedNode _ y) = compare x y
+  compare Inf (EvaluatedNode _ _) = GT
+  compare NegInf (EvaluatedNode _ _) = LT
+  compare (EvaluatedNode _ _) Inf = LT
+  compare (EvaluatedNode _ _) NegInf = GT
+  compare Inf _ = error "Internal error: Cannot compare Inf with non-EvaluatedNode"
+  compare NegInf _ = error "Interal error: Cannot compare NegInf with non-EvaluatedNode"
+
+evalNode :: GameState -> ABNode Double
 evalNode g = EvaluatedNode g $ evaluate g
 
-getState :: EvaluatedNode a -> GameState
+getState :: ABNode a -> GameState
 getState (EvaluatedNode g _) = g
+getState _ = error "Internal error: cannot get state of Inf or NegInf node"
 
 unfoldGame :: GameState -> (GameState, [GameState])
 unfoldGame game = (game, moveGen game)
@@ -29,15 +36,29 @@ prune :: Int -> Tree a -> Tree a
 prune 0 (Node g _)  = Node g []
 prune n (Node g sg) = Node g (map (prune $ n - 1) sg)
 
-minimize :: (Ord a) => Tree a -> a
-minimize (Node a []) = a
-minimize (Node a as) = minimum (map maximize as)
+minimize' :: (Ord a) => ABNode a -> ABNode a -> [Tree (ABNode a)] -> ABNode a
+minimize' _ _ []                      = error "Internal error: minimize' on [] illegal"
+minimize' alpha beta [x]              = maximize alpha beta x
+minimize' alpha beta (n@(Node x _):xs) = if newBeta <= alpha then newNode else minimize' alpha newBeta xs
+  where newNode = min x $ maximize alpha beta n
+        newBeta = min beta newNode
 
-maximize :: (Ord a) => Tree a -> a
-maximize (Node a []) = a
-maximize (Node a as) = maximum (map minimize as)
+minimize :: (Ord a) =>  ABNode a -> ABNode a -> Tree (ABNode a) -> ABNode a
+minimize _ _ (Node a []) = a
+minimize alpha beta (Node _ children) = minimize' alpha beta children
+
+maximize' :: (Ord a) => ABNode a -> ABNode a -> [Tree (ABNode a)] -> ABNode a
+maximize' _ _ []                      = error "Internal error: maximixe' on [] ilegal"
+maximize' alpha beta [x]              = minimize alpha beta x
+maximize' alpha beta (n@(Node x _):xs) = if newAlpha >= beta then newNode else maximize' newAlpha beta xs
+  where newNode = max x $ maximize alpha beta n
+        newAlpha = max alpha x
+
+maximize :: (Ord a) =>  ABNode a -> ABNode a -> Tree (ABNode a) -> ABNode a
+maximize _ _ (Node a []) = a
+maximize alpha beta (Node _ children) = maximize' alpha beta children
 
 search :: GameState -> Int -> Move
 search g depth = head . madeMoves $ getState bestNode
   where minmax   = if toMove g == White then maximize else minimize
-        bestNode = minmax . fmap evalNode . prune depth . unfoldTree unfoldGame $ g
+        bestNode = minmax NegInf Inf . fmap evalNode . prune depth . unfoldTree unfoldGame $ g
