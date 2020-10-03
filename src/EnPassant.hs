8d2885164e2c99ea7a6888581dd20c42a96cba2a
Matthew Leach <matthew@mattleach.net>
2020-10-03 22:30:20 +0100

    [EnPassant] fix shadow bindings

--- src/EnPassant.hs
+++ src/EnPassant.hs
@@ -16,11 +16,11 @@ defaultState = EnPassant Nothing
 
 -- Given a move, update the EP state.
 update :: PieceKind -> Move -> EnPassant
-update Pawn (Move from to _) | fr == R7 && tr == R5 = EnPassant $ Just (ff,Black)
-                             | fr == R2 && tr == R4 = EnPassant $ Just (ff,White)
-  where fr = locToRank from
-        tr = locToRank to
-        ff = locToFile from
+update Pawn m | fr == R7 && tr == R5 = EnPassant $ Just (ff,Black)
+              | fr == R2 && tr == R4 = EnPassant $ Just (ff,White)
+  where fr = locToRank $ from m
+        tr = locToRank $ to m
+        ff = locToFile $ from m
 
 update _ _ = EnPassant Nothing
 
@@ -30,9 +30,9 @@ isEPLocus (EnPassant (Just (file,c))) l = locToFR l == l'
   where rank = if c == White then R3 else R6
         l'   = (file,rank)
 
--- If a piece attacks `to`, given EnPassant, return the locus of the maybe
+-- If a piece attacks `attack`, given EnPassant, return the locus of the maybe
 -- captured Pawn.
 captureLoc :: EnPassant -> Locus -> Maybe Locus
 captureLoc (EnPassant Nothing) _ = Nothing
-captureLoc ep@(EnPassant (Just (file,c))) to = if isEPLocus ep to then Just (frToLoc (file,rank)) else Nothing
+captureLoc ep@(EnPassant (Just (file,c))) attack = if isEPLocus ep attack then Just (frToLoc (file,rank)) else Nothing
   where rank = if c == White then R4 else R5
