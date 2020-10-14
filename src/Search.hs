6a4edef6f8c8782072e4ef81440888907f3a8ee5
Matthew Leach <matthew@mattleach.net>
2020-10-14 17:30:27 +0100

    [Search] implement negamax with alpha beta

--- src/Search.hs
+++ src/Search.hs
@@ -6,28 +6,23 @@ import Move
 import Data.Tree
 import MoveGen
 import Evaluate
+import Data.List
+import Data.Ord
+import Control.Monad
+import Data.Either.Extra
 
-data ABNode a = EvaluatedNode GameState a | Inf | NegInf
+data SearchState = SearchState
+  { colour :: Double
+  , alpha :: Double
+  , beta :: Double }
+  deriving(Show)
 
-instance (Eq a) => Eq (ABNode a) where
-  (==) (EvaluatedNode _ x) (EvaluatedNode _ y) = x == y
-  (==) _ _    = False
+data EvaluatedNode = EvaluatedNode
+  { eval ::Double
+  , state :: GameState }
 
-instance (Ord a) => Ord (ABNode a) where
-  compare (EvaluatedNode _ x) (EvaluatedNode _ y) = compare x y
-  compare Inf (EvaluatedNode _ _) = GT
-  compare NegInf (EvaluatedNode _ _) = LT
-  compare (EvaluatedNode _ _) Inf = LT
-  compare (EvaluatedNode _ _) NegInf = GT
-  compare Inf _ = error "Internal error: Cannot compare Inf with non-EvaluatedNode"
-  compare NegInf _ = error "Interal error: Cannot compare NegInf with non-EvaluatedNode"
-
-evalNode :: GameState -> ABNode Double
-evalNode g = EvaluatedNode g $ evaluate g
-
-getState :: ABNode a -> GameState
-getState (EvaluatedNode g _) = g
-getState _ = error "Internal error: cannot get state of Inf or NegInf node"
+inf :: Double
+inf = read "Infinity"
 
 unfoldGame :: GameState -> (GameState, [GameState])
 unfoldGame game = (game, moveGen game)
@@ -36,29 +31,31 @@ prune :: Int -> Tree a -> Tree a
 prune 0 (Node g _)  = Node g []
 prune n (Node g sg) = Node g (map (prune $ n - 1) sg)
 
-minimize' :: (Ord a) => ABNode a -> ABNode a -> [Tree (ABNode a)] -> ABNode a
-minimize' _ _ []                      = error "Internal error: minimize' on [] illegal"
-minimize' alpha beta [x]              = maximize alpha beta x
-minimize' alpha beta (n@(Node x _):xs) = if newBeta <= alpha then newNode else minimize' alpha newBeta xs
-  where newNode = min x $ maximize alpha beta n
-        newBeta = min beta newNode
+abDescend :: SearchState -> Tree GameState -> EvaluatedNode
+abDescend s n@(Node g _) = EvaluatedNode v g
+  where flipState = SearchState (-colour s) (-beta s) (-alpha s)
+        v         = - (eval $ negaab flipState n)
 
-minimize :: (Ord a) =>  ABNode a -> ABNode a -> Tree (ABNode a) -> ABNode a
-minimize _ _ (Node a []) = a
-minimize alpha beta (Node _ children) = minimize' alpha beta children
+updateState :: SearchState -> EvaluatedNode -> SearchState
+updateState s n = SearchState (colour s) (max (eval n) $ alpha s) (beta s)
 
-maximize' :: (Ord a) => ABNode a -> ABNode a -> [Tree (ABNode a)] -> ABNode a
-maximize' _ _ []                      = error "Internal error: maximixe' on [] ilegal"
-maximize' alpha beta [x]              = minimize alpha beta x
-maximize' alpha beta (n@(Node x _):xs) = if newAlpha >= beta then newNode else maximize' newAlpha beta xs
-  where newNode = max x $ maximize alpha beta n
-        newAlpha = max alpha x
+type ScanState = (SearchState, EvaluatedNode)
 
-maximize :: (Ord a) =>  ABNode a -> ABNode a -> Tree (ABNode a) -> ABNode a
-maximize _ _ (Node a []) = a
-maximize alpha beta (Node _ children) = maximize' alpha beta children
+abScan :: ScanState -> Tree GameState -> Either ScanState ScanState
+abScan (s, n) child = if alpha newState >= beta newState then Left y else Right y
+  where newNode  = abDescend s child
+        bestNode = maximumBy (comparing eval) [n, newNode]
+        newState = updateState s bestNode
+        y        = (newState, bestNode)
+
+negaab :: SearchState -> Tree GameState -> EvaluatedNode
+negaab s (Node g []) = EvaluatedNode (colour s * evaluate g) g
+negaab s (Node _ (child:children)) = snd $ fromEither $ foldM abScan initialState children
+  where firstNode    = abDescend s child
+        initialState = (updateState s firstNode, firstNode)
 
 search :: GameState -> Int -> Move
-search g depth = head . madeMoves $ getState bestNode
-  where minmax   = if toMove g == White then maximize else minimize
-        bestNode = minmax NegInf Inf . fmap evalNode . prune depth . unfoldTree unfoldGame $ g
+search g depth   = head . madeMoves $ state bestNode
+  where s        = if toMove g == White then SearchState 1 (-inf) inf
+                                        else SearchState (-1) inf (-inf)
+        bestNode = negaab s . prune depth . unfoldTree unfoldGame $ g
