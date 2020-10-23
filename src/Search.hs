24885dac4161fe2f2af1ba9778f122e41c881bb8
Matthew Leach <matthew@mattleach.net>
2020-10-23 19:07:01 +0100

    [Search] fix and simplify
    Fix and simplify the alpha beta search as it currently will select
    pruned nodes as the pv.

--- src/Search.hs
+++ src/Search.hs
@@ -12,10 +12,8 @@ import Control.Monad
 import Data.Either.Extra
 
 data SearchState = SearchState
-  { colour :: Double
-  , alpha :: Double
+  { alpha :: Double
   , beta :: Double }
-  deriving(Show)
 
 data EvaluatedNode = EvaluatedNode
   { eval ::Double
@@ -32,34 +30,38 @@ prune 0 (Node g _)  = Node g []
 prune n (Node g sg) = Node g (map (prune $ n - 1) sg)
 
 abDescend :: SearchState -> Tree GameState -> EvaluatedNode
-abDescend s n = EvaluatedNode (- eval en) (state en)
-  where flipState = SearchState (-colour s) (-beta s) (-alpha s)
-        en =  negaab flipState n
+abDescend s n = EvaluatedNode (-eval nextNode) (state nextNode)
+  where flipState = SearchState (negate . beta $ s) (negate . alpha $ s)
+        nextNode = negaab flipState n
 
-updateState :: SearchState -> EvaluatedNode -> SearchState
-updateState s n = SearchState (colour s) (max (eval n) $ alpha s) (beta s)
+updateState :: SearchState -> Double -> SearchState
+updateState s score = SearchState (max score $ alpha s) (beta s)
 
 type ScanState = (SearchState, EvaluatedNode)
 
+
+selectBestNode :: (a -> a -> Ordering) -> a -> a -> a
+selectBestNode cmp a b = if cmp a b == EQ then a else maxNode
+  where maxNode = maximumBy cmp [a, b]
+
 abScan :: ScanState -> Tree GameState -> Either ScanState ScanState
-abScan (s, n) child = if alpha newState >= beta newState then Left y else Right y
+abScan  (s, n) child = if alpha newState >= beta newState then Left y else Right y
   where newNode  = abDescend s child
-        bestNode = maximumBy (comparing eval) [n, newNode]
-        newState = updateState s bestNode
+        bestNode = selectBestNode (comparing eval) n newNode
+        newState = updateState s $ eval bestNode
         y        = (newState, bestNode)
 
 negaab :: SearchState -> Tree GameState -> EvaluatedNode
-negaab s (Node g []) = EvaluatedNode (colour s * evaluate g) g
+negaab _ (Node g []) = EvaluatedNode ((if toMove g == White then id else negate) . evaluate $ g) g
 negaab s (Node _ (child:children)) = snd $ fromEither $ foldM abScan initialState children
   where firstNode    = abDescend s child
-        initialState = (updateState s firstNode, firstNode)
+        initialState = (updateState s $ eval firstNode, firstNode)
 
 getPV :: GameState -> GameState -> [Move]
 getPV g bestNode = moveList bestNode \\ moveList g
   where moveList = reverse . madeMoves
 
 search :: GameState -> Int -> (Double, [Move])
-search g depth = (eval bestNode, getPV g $ state bestNode)
-  where s = if toMove g == White then SearchState 1 (-inf) inf
-                                 else SearchState (-1) inf (-inf)
+search g depth = (evaluate . state $ bestNode, getPV g $ state bestNode)
+  where s = SearchState (-inf) inf
         bestNode = negaab s . prune depth . unfoldTree unfoldGame $ g
